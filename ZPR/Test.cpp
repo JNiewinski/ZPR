bfa93363e72ecb0fd14ba0db7cbee9787a800461
jniewinski <jakubniewinski@yahoo.com>
2019-11-30 21:40:07 +0100

    first test

---
+++ ZPR/Test.cpp
@@ -0,0 +1,12 @@
+
+#define BOOST_TEST_MODULE MyTest
+#include <boost/test/unit_test.hpp>
+
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(SimpleTestTools)
+{
+	BOOST_CHECK(1 == 1);
+	BOOST_REQUIRE(true);
+}
